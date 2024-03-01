300838dce4c73231f08f53a14d9e1f18039ad6c2
Joanne Santhosh <joanne.santhosh@mths.ca>
2024-03-01 13:56:31 +0000

    fixed code

--- Unit1-04/index.ts
+++ Unit1-04/index.ts
@@ -10,30 +10,30 @@ import { createPrompt } from 'bun-promptx'
 
 // input
 const randomNumber = Math.floor(Math.random() * 6)
-let guesses: number = 0
+let guesses = number = 0
 
 // while loop
 while (true) {
   const userGuessString = prompt("Guess a number between 1 - 6:")
   if (userGuessString == null) {
     break 
-}
+  }
   const userGuessInt = parseInt(userGuessString)
   if (isNaN(userGuessInt) == true || userGuessInt < 0) {
-   console.log("\nInvalid input.")
-} else {
-// process
-guesses++
-if (userGuessInt !== randomNumber) {
-  if (userGuessInt < randomNumber) {
-    console.log("Your guess is too low. Try again.")
+    console.log("\nInvalid input.")
   } else {
-    console.log("Your guess is too high. Try again.")
+    // process
+    guesses++
+    if (userGuessInt !== randomNumber) {
+      if (userGuessInt < randomNumber) {
+        console.log("Your guess is too low. Try again.")
+      } else {
+        console.log("Your guess is too high. Try again.")
+      }
+    } else {
+      console.log(`You correctly guessed the number in ${guesses} tries!`)
+      break
+    }
   }
-} else {
-  console.log(`You correctly guessed the number in ${guesses} tries!`)
-  break
-  }
- }
 }
 console.log("\nDone.")
